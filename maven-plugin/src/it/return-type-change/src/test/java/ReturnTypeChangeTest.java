07cbe06e601d7b9fb17cd808b9394406d7006ac3
Matt Brown <mattbrown@spotify.com>
2021-04-08 14:06:32 -0400

    fix failure in return-type-change integration test
    This test looks like it has always failed when run on Java 11, but we
    did not notice for a while because the CI pipeline was not running any
    of these tests (#130).
    
    The cause seems to be that the NoSuchMethodError getMessage() return
    value has changed.

--- maven-plugin/src/it/return-type-change/src/test/java/ReturnTypeChangeTest.java
+++ maven-plugin/src/it/return-type-change/src/test/java/ReturnTypeChangeTest.java
@@ -7,6 +7,13 @@ public class ReturnTypeChangeTest {
   public void shouldThrowError() throws Exception {
     Throwable ex = Assert.assertThrows(NoSuchMethodError.class,
         () -> ReturnTypeChange.main(new String[0]));
-    Assert.assertTrue(ex.getMessage().contains(")Ljava/lang/String;"));
+
+    // the message for NoSuchMethodError seems to have changed after Java 8
+    String message = System.getProperty("java.vm.specification.version").startsWith("1.8")
+                     ? "SpecificReturnType.aMethod(Ljava/lang/String;)Ljava/lang/String;"
+                     : "'java.lang.String SpecificReturnType.aMethod(java.lang.String)'";
+
+    Assert.assertEquals("Exception message: " + ex.getMessage(),
+        ex.getMessage(), message);
   }
 }
