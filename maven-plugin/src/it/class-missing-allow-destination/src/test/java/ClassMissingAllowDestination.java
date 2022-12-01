9d5391a2e4c8cd17667189d9cf1969967f7d6c79
Claire McGinty <clairem@spotify.com>
2022-12-01 14:27:33 -0500

    IT test

---
+++ maven-plugin/src/it/class-missing-allow-destination/src/test/java/ClassMissingAllowDestination.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ClassMissingAllowDestinationTest {
+
+  @Test
+  public void shouldThrowError() throws Exception {
+    Throwable ex = Assert.assertThrows(NoClassDefFoundError.class, () -> ClassMissingAllowDestination.main(new String[0]));
+    Assert.assertTrue(ex.getMessage().contains("WillGoAway"));
+  }
+}
